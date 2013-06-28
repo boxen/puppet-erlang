785451fb15eb0315330e30aa6347fee7fc174db9
Christopher Petersen <chris@petersen.io>
2013-06-28 15:28:10 -0700

    Updated specs to match

--- spec/classes/erlang_spec.rb
+++ spec/classes/erlang_spec.rb
@@ -1,14 +1,10 @@
 require 'spec_helper'
 
 describe 'erlang' do
-  it { should include_class('autoconf') }
-  it { should include_class('libtool') }
-
   it do
-    should contain_package('erlang').with({
-      :require => ['Package[automake]', 'Package[libtool]'],
+    should contain_package('Erlang').with({
+      :provider => 'pkgdmg',
+      :source   => 'https://elearning.erlang-solutions.com/couchdb/rbingen_adapter/package_erlang_R16B01-1_kgadek_2013.06.18_14:30:24/Erlang_R16B01_x64.dmg'
     })
   end
-
-  it { should contain_package('automake').with_require('Package[autoconf]') }
 end
